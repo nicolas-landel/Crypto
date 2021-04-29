0369f7cf499f7157633e09706a264a43990e7feb
Nicolas LAndel <nicolas.landel@hotmail.com>
2021-04-30 01:12:05 +0200

    Refactor into a class

--- main.py
+++ main.py
@@ -27,58 +27,54 @@ class CoinbaseWalletAuth(AuthBase):
         return request
 
 
-def init_currency_dic(account_data):
-    currency_data = {}
-    
-    currency_data = {
-        currency_data['currency']['code']: currency_data['balance']['amount']
-            for currency_data in account_data['data'] if float(currency_data['balance']['amount']) != 0
-    }
-    return currency_data
-
-
-def create_currency_value_dic(currency_amount_dic, api_url, auth):
-    data_dic = {'time': datetime.datetime.now().strftime('%H:%M %d-%m-%Y')}
-
-    for i, cur in enumerate(list(currency_amount_dic.keys())):
-        query = f'prices/{cur}-EUR/buy'
-        request_val_price = requests.get(api_url + query, auth=auth)
-        json_price = request_val_price.json()
-        price = round(float(json_price['data']['amount']) * float(currency_amount_dic.get(cur)), 2)
-        # Remove if lower than 1 EUR
-        if price > 1:  
-            data_dic[cur] =  price
-    
-    return data_dic
-
-
-
 
 class ProcessData():
     def __init__(self, auth, api_url):
         self.auth = auth
         self.api_url = api_url
-        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        self.account_data = self.parse_account_data()
+        self.currency_data = self.init_currency_dic()
+        self.data_dic = self.create_currency_value_dic()
+        self.__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
         # self.file = open(os.path.join(__location__, 'data.csv'), 'a+', encoding="utf-8")
         self.file_data = pd.read_csv("data.csv")
     
     def parse_account_data(self):
         # Fetch data from the account (amount of each currencies)
-        self.account_data = requests.get(self.api_url + 'accounts', auth=self.auth).json()  
+        return requests.get(self.api_url + 'accounts', auth=self.auth).json()
+
+    def init_currency_dic(self):
+        currency_data = {}
+        currency_data = {
+            currency_data['currency']['code']: currency_data['balance']['amount']
+                for currency_data in self.account_data['data'] if float(currency_data['balance']['amount']) != 0
+        }
+        return currency_data
     
+    def create_currency_value_dic(self):
+        data_dic = {'time': datetime.datetime.now().strftime('%H:%M %d-%m-%Y')}
+        for i, cur in enumerate(list(self.currency_data.keys())):
+            query = f'prices/{cur}-EUR/buy'
+            request_val_price = requests.get(self.api_url + query, auth=self.auth)
+            json_price = request_val_price.json()
+            price = round(float(json_price['data']['amount']) * float(self.currency_data.get(cur)), 2)
+            # Remove if lower than 1 EUR, mainly for DAI
+            if price > 1:  
+                data_dic[cur] =  price
+        
+        return data_dic
+    
+    def save_in_csv(self):
+        '''Save the new data in the csv'''
+        # directory = os.path.dirname(sys.argv[0])
+        if not os.path.isfile('data.csv'):
+            print(f"File path 'data.csv' does not exist. Exiting...")
+            sys.exit()
 
-
-def save_in_csv(data_dic):
-    '''Save the new data in the csv'''
-    # directory = os.path.dirname(sys.argv[0])
-    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    if not os.path.isfile('data.csv'):
-        print(f"File path 'data.csv' does not exist. Exiting...")
-        sys.exit()
-
-    with open(os.path.join(__location__, 'data.csv'), 'a+', encoding="utf-8") as file_data:
-        csv_writer = csv.writer(file_data)
-        csv_writer.writerow(list(data_dic.values()))
+        with open(os.path.join(self.__location__, 'data.csv'), 'a+', encoding="utf-8") as file_data:
+            csv_writer = csv.writer(file_data)
+            csv_writer.writerow(list(self.data_dic.values()))
+    
 
 
 original_values = {
@@ -108,10 +104,8 @@ def run_pipeline():
     API_SECRET = os.getenv("secret")
     api_url = 'https://api.coinbase.com/v2/'
     auth = CoinbaseWalletAuth(API_KEY, API_SECRET)  # Config the connection to coinbase
-    
-    currency_amount_dic = init_currency_dic(account_data)
-    data_dic = create_currency_value_dic(currency_amount_dic, api_url, auth)  # Format the data into a dic, with currency tags as keys and amount (EUR) as values
-    save_in_csv(data_dic)
+    process = ProcessData(auth, api_url)
+    process.save_in_csv()
 
 if __name__ == '__main__':
     run_pipeline()
