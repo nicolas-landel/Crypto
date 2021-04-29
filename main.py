12a1981d8e0909add5d1f66bcfca4d62dc7c868f
Nicolas LAndel <nicolas.landel@hotmail.com>
2021-04-30 00:47:49 +0200

    Modification on the main

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ def init_currency_dic(account_data):
     currency_data = {}
     
     currency_data = {
-        currency_data['currency']['code']: currency_data['balance']['amount'] for currency_data in account_data['data'] if float(currency_data['balance']['amount']) != 0
+        currency_data['currency']['code']: currency_data['balance']['amount']
+            for currency_data in account_data['data'] if float(currency_data['balance']['amount']) != 0
     }
     return currency_data
 
@@ -43,9 +44,28 @@ def create_currency_value_dic(currency_amount_dic, api_url, auth):
         query = f'prices/{cur}-EUR/buy'
         request_val_price = requests.get(api_url + query, auth=auth)
         json_price = request_val_price.json()
-        data_dic[cur] = round(float(json_price['data']['amount']) * float(currency_amount_dic.get(cur)), 2)
+        price = round(float(json_price['data']['amount']) * float(currency_amount_dic.get(cur)), 2)
+        # Remove if lower than 1 EUR
+        if price > 1:  
+            data_dic[cur] =  price
     
     return data_dic
+
+
+
+
+class ProcessData():
+    def __init__(self, auth, api_url):
+        self.auth = auth
+        self.api_url = api_url
+        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        # self.file = open(os.path.join(__location__, 'data.csv'), 'a+', encoding="utf-8")
+        self.file_data = pd.read_csv("data.csv")
+    
+    def parse_account_data(self):
+        # Fetch data from the account (amount of each currencies)
+        self.account_data = requests.get(self.api_url + 'accounts', auth=self.auth).json()  
+    
 
 
 def save_in_csv(data_dic):
@@ -61,13 +81,34 @@ def save_in_csv(data_dic):
         csv_writer.writerow(list(data_dic.values()))
 
 
+original_values = {
+    "DASH": 50,
+    "NKN": 50,
+    "ANKR": 50,
+    "KNC": 50,
+    "ADA": 50,
+    "MANA": 20,
+    "SKL": 32,
+    "MATIC": 75,
+    "XLM": 50,
+    "UNI": 100,
+    "LRC": 100,
+    "CVC": 100,
+    "ETC": 150,
+    "XTZ": 50,
+    "GRT": 90,
+    "ALGO": 100,
+    "BCH": 71,
+}
+
+
 def run_pipeline():
     load_dotenv()
     API_KEY = os.getenv("key")
     API_SECRET = os.getenv("secret")
     api_url = 'https://api.coinbase.com/v2/'
     auth = CoinbaseWalletAuth(API_KEY, API_SECRET)  # Config the connection to coinbase
-    account_data = requests.get(api_url + 'accounts', auth=auth).json()  # Fetch data from the account (amount of each currencies)
+    
     currency_amount_dic = init_currency_dic(account_data)
     data_dic = create_currency_value_dic(currency_amount_dic, api_url, auth)  # Format the data into a dic, with currency tags as keys and amount (EUR) as values
     save_in_csv(data_dic)
