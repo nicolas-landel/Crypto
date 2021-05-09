9695949ab6656ceed0f6a0b3d72fbcebfd4afc67
Nicolas LAndel <nicolas.landel@hotmail.com>
2021-05-09 23:57:18 +0200

    Update the scripts

--- scripts/branch.sh
+++ scripts/branch.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-create_new_branch() {
+create_fetch_branch() {
     git checkout -b fetch_data
     git push origin -u fetch_data
-    branch=fetch_data
 }
 
 create_new_pr() {
@@ -18,13 +17,22 @@ create_new_pr() {
     fi
 }
 
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-if [[ "$BRANCH" == "main" ]]; then
+branch_fetch="fetch_data"
+current_branch="$(git rev-parse --abbrev-ref HEAD)"
+if [[ "$current_branch" == "main" ]]; then
+# Initialisation of the process
 # If branch main, ie not current PR so create one
     echo "Current branch is main"
-    create_new_branch
+    if [ `git branch --list $branch_fetch` ]; then
+        echo "Branch name $branch_fetch already exists."
+        git checkout $branch_fetch
+        git fetch && git pull
+    else
+        create_fetch_branch
+    fi
 
 else 
+# Normal process
 # Current branch is not main so it is fetch_data -> need to merge PR and create new one
     current_branch=$(git rev-parse --abbrev-ref HEAD)
     echo "Current branch is not main, it is $current_branch"
@@ -35,9 +43,10 @@ else
     git checkout main
     git fetch && git pull
     git merge --no-ff $current_branch
+    echo "Merge of $current_branch"
     git branch -D $current_branch
     # Create again a new branch which will receive the updates of the data.csv
-    create_new_branch
+    create_fetch_branch
 
 fi
 
