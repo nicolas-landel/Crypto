3fe3d04fade6e8741e6f39a5e062bef0a9ef0d98
Nicolas Landel <33714438+nicolas-landel@users.noreply.github.com>
2021-05-10 18:21:52 +0200

    Update switch_branch.sh

--- scripts/switch_branch.sh
+++ scripts/switch_branch.sh
@@ -2,6 +2,7 @@
 
 echo "$(git status)"
 branch_fetch="fetch_data"
+git fetch && git pull
 if [[ `git branch --list $branch_fetch` ]]; then
     echo "Branch name $branch_fetch exists, checkout to it."
     current_branch="$(git rev-parse --abbrev-ref HEAD)"
